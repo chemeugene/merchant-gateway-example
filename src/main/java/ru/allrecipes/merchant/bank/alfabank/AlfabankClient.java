7caeb30420cce7b0edc8e634639c3c782e23ac8c
Eugene Ivlev <chemeugene@gmail.com>
2018-03-20 00:48:13 +0300

    fixes #1. New generation

--- src/main/java/ru/allrecipes/merchant/bank/alfabank/AlfabankClient.java
+++ src/main/java/ru/allrecipes/merchant/bank/alfabank/AlfabankClient.java
@@ -1,7 +1,11 @@
 package ru.allrecipes.merchant.bank.alfabank;
 
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+import org.springframework.ws.soap.client.core.SoapActionCallback;
 
 public class AlfabankClient extends WebServiceGatewaySupport {
 
+  /*public VerifyEnrollmentResponse verifyEnrollment() {
+    VerifyEnrollmentResponse response = getWebServiceTemplate().marshalSendAndReceive(null, new SoapActionCallback(soapAction));
+  }*/
 }
